f3361bc7f1d2c0c24b2a4452bdd63d659b382ece
aniketjumde <aniketjumde55@gmail.com>
2024-10-02 20:44:11 +0530

    First Commit

--- Data Structure and Algorithms/1.Searchin and Sorting/Merge_sort.c
+++ Data Structure and Algorithms/1.Searchin and Sorting/Merge_sort.c
@@ -1,19 +1,18 @@
-// 15 20 40 50 5 10 30 35
-
 #include<stdio.h>
+#include<stdlib.h>
 
-void accept_array(int a[],int n)
+void accept(int a[],int n)
 {
     for(int i=0;i<n;i++)
     {
         printf("Enter the data :");
         scanf("%d",&a[i]);
     }
+    printf("\n");
 }
 
-void display_array(int a[],int n)
+void display(int a[],int n)
 {
-    
     for(int i=0;i<n;i++)
     {
         printf("%d ",a[i]);
@@ -21,71 +20,90 @@ void display_array(int a[],int n)
     printf("\n");
 }
 
-void merge(int a[],int lb,int mid ,int ub)
+void merge(int a[],int lb,int mid,int ub)
 {
-    int temp[100];
+    int temp[100]; //array used for merging
     int i,j,k;
-    i=lb;
-    j=mid+1;
-    k=lb;
+    i=lb;  // beging for first array
+    j=mid+1; //beging for second array
+    k=0;
+
     while(i<=mid && j<=ub)
     {
         if(a[i]<a[j])
         {
             temp[k]=a[i];
-            k++,i++;
+            k++;
+            i++;
         }
         else
         {
             temp[k]=a[j];
-            k++,j++;
+            k++;
+            j++;
         }
     }
 
     while(i<=mid)
-    {
+    {                       // Copy remaining element in first list
         temp[k]=a[i];
         k++;
         i++;
     }
 
     while(j<=ub)
-    {
+    {                       //Copy remaining element in second list
         temp[k]=a[j];
         k++;
         j++;
     }
 
-    for(k=lb;k<=ub;k++)
+    k=0;
+    for(i=lb;i<=ub;i++,k++) //tranfer element from temp array to array 
     {
-        a[k]=temp[k];
+        a[i]=temp[k];
     }
 }
 
 void merge_sort(int a[],int lb,int ub)
 {
     int mid;
-    mid=(lb + ub)/2;
-    merge_sort(a,lb,mid);
-    merge_sort(a,mid,ub);
-    merge(a,lb,mid,ub);
-
+    if(lb<ub)
+    {
+        mid=(lb+ub)/2;
+        merge_sort(a,lb,mid);
+        merge_sort(a,mid+1,ub);
+        merge(a,lb,mid,ub);
+    }
 }
 
 int main()
 {
     int n;
-
-    printf("How many number you want to store : ");
+    int *a;
+    
+    printf("How many number you want to store :");
     scanf("%d",&n);
 
-    int a[n];
+    a=(int*)malloc(n*sizeof(int));
 
-    accept_array(a,n);
-    printf("Array is : ");
-    display_array(a,n);
-    printf("After Merge sort is : ");
+    if(a==NULL)
+    {
+        printf("Insuffiecent Memory..!");
+        exit(0);
+    }
+
+    accept(a,n);
+    printf("Before Sort : ");
+    display(a,n);
+
     merge_sort(a,0,n-1);
-    display_array(a,n);
+
+    printf("After Sort : ");
+    display(a,n);
+
+    free(a);
     
+    return 0;
+
 }
--- Data Structure and Algorithms/1.Searchin and Sorting/quick_sort.c
+++ Data Structure and Algorithms/1.Searchin and Sorting/quick_sort.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void accept(int a[],int n)
 {
@@ -17,19 +18,79 @@ void display(int a[],int n)
     }
 }
 
-void partition(int a[],int n,int lb,int up)
+int partition(int a[],int lb,int ub)
 {
+    int pivot,i,j;
+    int t;
+    pivot=a[lb];
+    i=lb+1;
+    j=ub;
+
+    while (i<=j)
+    {
+        while(a[i]<pivot)
+        {
+            i++;
+        }
+        
+        while (a[j]>pivot)
+        {
+            j--;
+        }
+
+        if(i<j)
+        {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+        }
+        
+    }
+    a[lb]=a[j];
+    a[j]=pivot;
     
+    return j;
+    
+}
+
+void quick_sort(int a[],int lb,int ub)
+{
+    int j;
+    if(lb<ub)
+    {
+        j=partition(a,lb,ub);
+        quick_sort(a,lb,j-1);
+        quick_sort(a,j+1,ub);
+    }
 }
 
 int main()
 {
     int n;
+    int *a;
     printf("How many number you want to store :");
     scanf("%d",&n);
 
-    int a[100];
+    a=(int *)malloc(n*sizeof(int));
+
+    if(a==NULL)
+    {
+        printf("Insufficent memory..!");
+        exit(0);
+    }
+
     accept(a,n);
-    printf("Array is :");
+    printf("Before sort: ");
     display(a,n);
+
+    quick_sort(a,0,n-1);
+
+    printf("\n");
+    printf("After sort: ");
+    display(a,n);
+
+    free(a);
+
+    return 0;
+
 }
