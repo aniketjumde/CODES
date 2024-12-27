a0e6973551b537e7003ff08953e3ef83f167161e
aniketjumde <aniketjumde55@gmail.com>
2024-12-27 23:45:08 +0530

    First Commit

--- Python/14.Python_MYSQL/02_creating_database_table.py
+++ Python/14.Python_MYSQL/02_creating_database_table.py
@@ -5,7 +5,7 @@ con=mysql.connector.connect(host="localhost",port="3306",user="root",password="r
 c=con.cursor()
 
 c.execute("create table student(rno int primary key,name varchar(10),per float)")
-
+    
 con.commit()
 c.close()
 con.close()
