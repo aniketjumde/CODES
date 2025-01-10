aedb24f1ef8763aa1cc2572da3552aab7325a9d2
aniketjumde <aniketjumde55@gmail.com>
2025-01-11 00:10:42 +0530

    First Commit

---
+++ Core-Java(J2SE)/HelloWorld.java
@@ -0,0 +1,14 @@
+
+class HelloWorld {
+    public void printMessage() {
+        System.out.println("Hello, World!");
+    }
+}
+
+public class HelloWorldDemo {
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+
+        hello.printMessage();
+    }
+}
